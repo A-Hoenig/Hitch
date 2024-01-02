c9b5882db6fc1d0e1d31281005eeaeb1b8f02682
Alex Hoenig <delimey@pm.me>
2024-01-02 18:46:54 +0000

    link main landing page and prepare templates

--- hitch/settings.py
+++ hitch/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'hitch.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- rides/views.py
+++ rides/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def rides(request):
-    return HttpResponse('Hello Horld')
+    
+    return render(request, 'rides/rides.html')
