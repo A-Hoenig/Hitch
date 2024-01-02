31d542ec4c5610198b86290fcf13065b1dcf7777
Alex Hoenig <delimey@pm.me>
2024-01-02 11:42:15 +0000

    add new security keys and connect to heroku

--- hitch/settings.py
+++ hitch/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2_a(wp7o160p4$-7om0ihhek8zn&p)h2ql_-^k+97_9knt=+8q'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
