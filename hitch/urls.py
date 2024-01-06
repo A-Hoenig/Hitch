0dbef1dbc4c3196709c604eb00daa1cae024d6d4
Alex Hoenig <delimey@pm.me>
2024-01-06 13:58:05 +0000

    add user login and authentication

--- hitch/urls.py
+++ hitch/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rides import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('', views.rides, name='rides'),
     path('about/', views.about, name='about'),
     path('hitches/', views.hitches, name='hitches'),
