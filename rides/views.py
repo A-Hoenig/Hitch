15ab1323ccf5ec36d5eec057e09b78fb6554d4ea
Alex Hoenig <delimey@pm.me>
2024-01-11 09:42:58 +0000

    add login feedback welcome when logged in

--- rides/views.py
+++ rides/views.py
@@ -3,12 +3,18 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def rides(request):
-    return render(request, 'rides/rides.html')
+    return render(request, 'rides/rides.html', {
+        "username": request.user
+        })
 
 
 def hitches(request):
-    return render(request, 'rides/hitches.html')
+    return render(request, 'rides/hitches.html', {
+        "username": request.user
+        })
 
 
 def about(request):
-    return render(request, 'rides/about.html')
+    return render(request, 'rides/about.html', {
+        "username": request.user
+        })
