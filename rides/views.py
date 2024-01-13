a694e8f90b44ebfd1b59a81d4e26ee9b261e509d
Alex Hoenig <delimey@pm.me>
2024-01-13 14:18:33 +0000

    get list of vehicles ownder by user

--- rides/views.py
+++ rides/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from rides.models import CustomUser
+from django.shortcuts import render, HttpResponse, get_object_or_404, reverse
+from rides.models import CustomUser, Vehicle, User
 from rides.forms import UserForm, VehicleForm
 
 # Create your views here.
@@ -25,11 +25,8 @@ def about(request):
 
 
 def profile(request):
-    
     form = UserForm()
-    
     if request.method == "POST":
-        
         if form.is_valid:
             form = UserForm(request.POST)
             form.save()
@@ -40,16 +37,25 @@ def profile(request):
     context = {
             "username": request.user,
             "form": form,
-        }
-
+    }
     return render(request, 'rides/user_profile.html', context)
 
 
 def vehicles(request):
     form = VehicleForm()
+    
+    if request.method == "POST":
+        if form.is_valid:
+            form = VehicleForm(request.POST)
+            form.save()
+            return redirect('rides/vehicles.html')
+    else:
+        form = UserForm()
+        vehicles = Vehicle.objects.filter(owner=request.user)
     context = {
             "username": request.user,
             "form": form,
+            "vehicles": vehicles,
         }
 
     return render(request, 'rides/vehicles.html', context)
