06d68fccafdec75c612241b48a3dcda4dfcc5140
Alex Hoenig <delimey@pm.me>
2024-01-14 11:37:04 +0000

    force update static files

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -5,10 +5,30 @@ document.addEventListener('DOMContentLoaded', function() {
     // Add a click event listener to each anchor tag
     accordionLinks.forEach(function(link) {
       link.addEventListener('click', function(event) {
-        // Prevent the default action of the link
+        // Prevent the default action of the link - no accordion
         event.preventDefault();
         // Open the link's URL in the current tab
         window.location.href = link.getAttribute('href');
       });
     });
   });
+
+const exampleModal = document.getElementById('exampleModal')
+if (exampleModal) {
+  exampleModal.addEventListener('show.bs.modal', event => {
+    // Button that triggered the modal
+    const button = event.relatedTarget
+    // Extract info from data-bs-* attributes
+    const recipient = button.getAttribute('data-bs-whatever')
+    // If necessary, you could initiate an Ajax request here
+    // and then do the updating in a callback.
+
+    // Update the modal's content.
+    const modalTitle = exampleModal.querySelector('.modal-title')
+    const modalBodyInput = exampleModal.querySelector('.modal-body input')
+
+    modalTitle.textContent = `New message to ${recipient}`
+    modalBodyInput.value = recipient
+  })
+}
+
