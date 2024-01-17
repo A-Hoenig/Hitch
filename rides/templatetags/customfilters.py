6096b0133f3114e557a3b573cc5caed2a5d0b700
Alex Hoenig <delimey@pm.me>
2024-01-17 09:22:13 +0000

    create date and time filters HHMM, HMM, DMY

--- rides/templatetags/customfilters.py
+++ rides/templatetags/customfilters.py
@@ -2,7 +2,18 @@ from django import template
 register = template.Library()
 
 @register.filter(expects_localtime=True)
-def format_duration(td):
+def HMM(td):
     hours = td.seconds // 3600
     minutes = (td.seconds % 3600) // 60
     return '{:01d}:{:02d}'.format(hours, minutes)
+
+@register.filter
+def DMY(value):
+    return value.strftime('%d.%m.%y')
+
+@register.filter(expects_localtime=True)
+def HHMM(value):
+    return value.strftime('%H:%M')
+
+
+
