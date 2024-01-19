7ec0fb6a3f479b3c54c6b8561b4f9a766e79675e
Alex Hoenig <delimey@pm.me>
2024-01-19 10:01:55 +0000

    add locations tab to user profile page

--- hitch/urls.py
+++ hitch/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('hitches/', views.hitches, name='hitches'),
     path('user_profile/', views.profile, name='user_profile'),
     path('vehicles/', views.vehicles, name='vehicles'),
+    path('locations/', views.locations, name='locations'),
     path('user_trips/', views.trips, name='user_trips'),
     path('change-password/', PasswordChangeView.as_view(), name='password_change'),
 ]
--- rides/views.py
+++ rides/views.py
@@ -196,6 +196,67 @@ def trips(request):
     return render(request, 'rides/user_trips.html', context)
 
 
+# -------------------------------------------------------
+@login_required
+def locations(request):
+    form = VehicleForm()
+    filter_value = request.GET.get('status')
+    # Check if the user has a Date of Birth set
+    if request.user.DOB:
+        today = date.today()
+        born = request.user.DOB
+        age = today.year - born.year - ((today.month, today.day) < (born.month, born.day))
+    else:
+        age = None
+
+    if request.method == "POST":
+        if 'save' in request.POST:
+            form = VehicleForm(request.POST)
+            form.instance.owner = request.user
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Vehicle added successfully!')
+        elif 'delete' in request.POST:
+            pk = request.POST.get('delete')
+            vehicle = Vehicle.objects.get(id=pk)
+            vehicle.delete()
+            messages.success(request, 'Vehicle deleted sucessfully!')
+        elif 'update' in request.POST:
+            pk = request.POST.get('update')
+            # print(f'....starting update for {pk}')
+            vehicle = Vehicle.objects.get(id=pk)
+            post_data = request.POST.copy()
+            form = VehicleForm(request.POST, instance=vehicle)
+
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Vehicle updated sucessfully!')
+
+        return redirect('vehicles')
+
+    else:
+        form = VehicleForm()
+        # vehicles owned by the user
+        if filter_value in ['True', 'False']:
+            filter_value = filter_value == 'True'
+            vehicles = Vehicle.objects.filter(owner=request.user, status=filter_value).order_by('make')
+        else:
+            # Empty value = 'All'
+            vehicles = Vehicle.objects.filter(owner=request.user).order_by('make')
+            
+        # Create a list of forms for each vehicle instance
+        forms = [VehicleForm(instance=vehicle) for vehicle in vehicles]
+
+        context = {
+            "username": request.user,
+            "form": form,
+            "vehicles": zip(vehicles, forms),
+            "age": age,
+        }
+
+    return render(request, 'rides/locations.html', context)
+
+
 
 # -------------------------------------------------------
 @login_required
