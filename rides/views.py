f2a14e3315f9ee7bfde6fae0733da65ac5067c64
Alex Hoenig <delimey@pm.me>
2024-01-24 12:39:45 +0000

    add logo to about page

--- rides/views.py
+++ rides/views.py
@@ -131,7 +131,7 @@ def rides_view(request):
             if form.is_valid():
                 new_trip = form.save(commit=False)
                 new_trip.driver = request.user 
-                # new_trip.save()
+                new_trip.save()
                 messages.success(request, f'New trip to {new_trip.destination.city} created successfully! Thanks for sharing!')
                 return HttpResponseRedirect(request.path_info)
             else:
