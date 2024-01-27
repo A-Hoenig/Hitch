0423480f28ba1d842adeebe3fe6d9f3c41476f8e
Alex Hoenig <delimey@pm.me>
2024-01-27 10:54:58 +0000

    admin.py linted no errors

--- rides/admin.py
+++ rides/admin.py
@@ -2,70 +2,122 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.contrib.auth.admin import UserAdmin
-from .models import Region, Location, CustomUser, Purpose, Vehicle, User_rating, Trip, Hitch_Request, Stop_Type, Message 
+from .models import (
+    Region,
+    Location,
+    CustomUser,
+    Purpose,
+    Vehicle,
+    User_rating,
+    Trip,
+    Hitch_Request,
+    Stop_Type,
+    Message)
+
 
 class CustomUserAdmin(UserAdmin):
     list_display = (
         'username', 'first_name', 'last_name',
-        'gender', 'DOB', 'average_driver_rating','average_hitcher_rating'
+        'gender', 'DOB', 'average_driver_rating', 'average_hitcher_rating'
         )
 
     fieldsets = (
         (None, {
-            'fields': ('username', 'password', 'user_image')
-        }),
+            'fields': (
+                'username',
+                'password',
+                'user_image')}),
         ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email', 'gender','average_driver_rating','average_hitcher_rating')
-        }),
+            'fields': (
+                'first_name',
+                'last_name',
+                'email',
+                'gender',
+                'average_driver_rating',
+                'average_hitcher_rating')}),
         ('Hitch Driver Info', {
-            'fields': ('DOB', 'DL_date', 'adr_street', 'adr_city', 'adr_zip', 'adr_country', 'phone', 'contactable')
-        }),
+            'fields': (
+                'DOB',
+                'DL_date',
+                'adr_street',
+                'adr_city',
+                'adr_zip',
+                'adr_country',
+                'phone',
+                'contactable')}),
         ('Permissions', {
             'fields': (
-                'is_active', 'is_staff', 'is_superuser',
-                'groups', 'user_permissions'
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions'
                 )
         }),
         ('Important dates', {
-            'fields': ('last_login', 'date_joined')
+            'fields': (
+                'last_login',
+                'date_joined')
         })
-        
     )
 
     add_fieldsets = (
         (None, {
-            'fields': ('username', 'password1', 'password2')
-        }),        
+            'fields': (
+                'username',
+                'password1',
+                'password2')
+        }),
         ('Personal info', {
-            'fields': ('first_name', 'last_name', 'email', 'gender','average_driver_rating','average_hitcher_rating')
+            'fields': (
+                'first_name',
+                'last_name',
+                'email',
+                'gender',
+                'average_driver_rating',
+                'average_hitcher_rating')
         }),
         ('Hitch Driver Info', {
-            'fields': ('DOB', 'DL_date', 'adr_street', 'adr_city', 'adr_zip', 'adr_country', 'phone', 'contactable')
-        }),
+            'fields': (
+                'DOB',
+                'DL_date',
+                'adr_street',
+                'adr_city',
+                'adr_zip',
+                'adr_country',
+                'phone',
+                'contactable')}),
         ('Permissions', {
             'fields': (
-                'is_active', 'is_staff', 'is_superuser',
-                'groups', 'user_permissions'
-                )
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions')
         }),
         ('Important dates', {
-            'fields': ('last_login', 'date_joined')
+            'fields': (
+                'last_login',
+                'date_joined')
         })
-        
     )
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
+
 class RegionAdmin(admin.ModelAdmin):
-    list_display = ('id','region', 'date_created')
+    list_display = ('id', 'region', 'date_created')
     list_filter = ['region', 'date_created']
+
 
 admin.site.register(Region, RegionAdmin)
 
 
 class CustomLocationAdmin(admin.ModelAdmin):
-    list_display = ('id','region','name','city', 'date_created','input_by')
+    list_display = ('id', 'region', 'name', 'city', 'date_created', 'input_by')
     list_filter = ['region', 'date_created']
+
 
 admin.site.register(Location, CustomLocationAdmin)
 
@@ -77,6 +129,7 @@ class StopTypeAdmin(admin.ModelAdmin):
         return mark_safe(obj.stop_icon)
     icon_display.short_description = 'Icon'
 
+
 admin.site.register(Stop_Type, StopTypeAdmin)
 
 admin.site.register(Purpose)
@@ -87,8 +140,15 @@ admin.site.register(User_rating)
 
 
 class TripAdmin(admin.ModelAdmin):
-    list_display = ('id','driver','date_created_DMY', 'depart_date','depart_time','depart','destination',)
-    list_filter = ('region','driver',)
+    list_display = (
+        'id',
+        'driver',
+        'date_created_DMY',
+        'depart_date',
+        'depart_time',
+        'depart',
+        'destination',)
+    list_filter = ('region', 'driver',)
 
     # create some more readable date/time formats for datatable
     def date_created_DMY(self, obj):
@@ -103,6 +163,7 @@ class TripAdmin(admin.ModelAdmin):
         return obj.date_created.strftime("%H:%M")
     depart_time_HHMM.short_description = "At"
 
+
 admin.site.register(Trip, TripAdmin)
 
 
@@ -111,7 +172,7 @@ class Hitch_RequestAdmin(admin.ModelAdmin):
     def display_trip(self, obj):
         return obj.trip if obj.trip else "No drivers yet"
     display_trip.short_description = 'Linked Trip Details'
-    
+
     def get_driver(self, obj):
         if obj.trip:
             return obj.trip.driver.username
@@ -132,10 +193,18 @@ class Hitch_RequestAdmin(admin.ModelAdmin):
         return obj.date_created.strftime("%H:%M")
     depart_time_HHMM.short_description = "At"
 
-    list_display = ('id','date_created_YMD','is_public','pax_approved','display_trip', 'get_driver', 'hitcher', 'depart_date_YMD', 'depart_time_HHMM')
-    list_filter = ('region','is_public', 'hitcher' )
+    list_display = (
+        'id',
+        'date_created_YMD',
+        'is_public',
+        'pax_approved',
+        'display_trip',
+        'get_driver',
+        'hitcher',
+        'depart_date_YMD',
+        'depart_time_HHMM')
+    list_filter = ('region', 'is_public', 'hitcher')
 
-    
 
 admin.site.register(Hitch_Request, Hitch_RequestAdmin)
 
